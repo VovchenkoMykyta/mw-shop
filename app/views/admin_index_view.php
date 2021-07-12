b321b200110ef6f7de0907821ab72a018773c1ba
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-07-12 22:24:48 +0300

    modified look for admin panel

--- app/views/admin_index_view.php
+++ app/views/admin_index_view.php
@@ -1,27 +1,27 @@
-<div id="header">
-    <h2>Admin Index</h2>
-</div>
-<div id="lsb">
-    <ul>
-        <li>User
-            <ul>
-                <li><a href="<?= \core\Router::getUrl('view', 'add_user')?>">Add User</a></li>
-                <li><a href="<?= \core\Router::getUrl('view', 'all_users')?>">Users Info</a></li>
-            </ul>
-        </li>
-        <li>Product
-            <ul>
-                <li>All Products</li>
-            </ul>
-        </li>
-        <li>Category
-            <ul>
-                <li>All Categories</li>
-                <li>Add Category</li>
-            </ul>
-        </li>
-    </ul>
-</div>
-<div id="main">
-
-</div>
+<!--<div id="header">-->
+<!--    <h2>Admin Index</h2>-->
+<!--</div>-->
+<!--<div id="lsb">-->
+<!--    <ul>-->
+<!--        <li>User-->
+<!--            <ul>-->
+<!--                <li><a href="--><?//= \core\Router::getUrl('view', 'add_user')?><!--">Add User</a></li>-->
+<!--                <li><a href="--><?//= \core\Router::getUrl('view', 'all_users')?><!--">Users Info</a></li>-->
+<!--            </ul>-->
+<!--        </li>-->
+<!--        <li>Product-->
+<!--            <ul>-->
+<!--                <li>All Products</li>-->
+<!--            </ul>-->
+<!--        </li>-->
+<!--        <li>Category-->
+<!--            <ul>-->
+<!--                <li>All Categories</li>-->
+<!--                <li>Add Category</li>-->
+<!--            </ul>-->
+<!--        </li>-->
+<!--    </ul>-->
+<!--</div>-->
+<!--<div id="main">-->
+<!---->
+<!--</div>-->
--- app/views/template/admin_default_view.php
+++ app/views/template/admin_default_view.php
@@ -7,6 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <link rel="stylesheet" href="/css/index.css">
+        <link rel="stylesheet" href="/css/admin_default.css">
         <link rel="stylesheet" href="/css/<?=stristr($pageFile, '.', true)?>.css">
     </head>
     <body>
@@ -14,9 +15,32 @@
             <div>Admin panel</div>
         </header>
         <main>
-            <?php
-            include_once 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $pageFile;
-            ?>
+            <div id="lsb">
+                <ul>
+                    <li>User
+                        <ul>
+                            <li><a href="<?= \core\Router::getUrl('view', 'add_user')?>">Add User</a></li>
+                            <li><a href="<?= \core\Router::getUrl('view', 'all_users')?>">Users Info</a></li>
+                        </ul>
+                    </li>
+                    <li>Product
+                        <ul>
+                            <li>All Products</li>
+                        </ul>
+                    </li>
+                    <li>Category
+                        <ul>
+                            <li>All Categories</li>
+                            <li>Add Category</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div id="main">
+                <?php
+                include_once 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $pageFile;
+                ?>
+            </div>
         </main>
         <footer>
             <span><a href="https://github.com/VovchenkoMykyta/mw-shop.git">VovchenkoMykyta &copy;</a></span>
