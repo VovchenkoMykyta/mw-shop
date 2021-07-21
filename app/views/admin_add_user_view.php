d308f0d22b99c81af0c6befd4c3fc27ac4b78d8c
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-07-21 22:33:44 +0300

    upgrate form in add user

--- app/views/admin_add_user_view.php
+++ app/views/admin_add_user_view.php
@@ -1,15 +1,18 @@
 <form action="<?= \core\Router::getUrl('admin', 'addUser')?>" method="post">
-    <label>Login:
-        <input type="text" name="login" placeholder="Enter user login" required>
-    </label>
-    <label>Password:
-        <input type="password" name="pass" placeholder="Enter user password" required>
-    </label>
-    <label>Email:
-        <input type="email" name="email" placeholder="Enter email">
-    </label>
-    <label>Phone:
-        <input type="tel" name="phone" placeholder="Enter phone">
-    </label>
+    <fieldset>
+        <legend>Add user</legend>
+        <label>Login:
+            <input type="text" name="login" placeholder="Enter user login" required>
+        </label>
+        <label>Password:
+            <input type="password" name="pass" placeholder="Enter user password" required>
+        </label>
+        <label>Email:
+            <input type="email" name="email" placeholder="Enter email">
+        </label>
+        <label>Phone:
+            <input type="tel" name="phone" placeholder="Enter phone">
+        </label>
+    </fieldset>
     <input type="submit" value="Add user">
 </form>
