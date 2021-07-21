e3fe6343f49de3277b8defe30fd37f8e50d057a3
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-07-21 18:21:01 +0300

    edit show products on index page

--- app/views/index_index_view.php
+++ app/views/index_index_view.php
@@ -6,6 +6,11 @@ $prices = $model->getPrice();
 <div class="main_photo">
 <?php foreach ($images as $key => $image): ?>
     <div>
+        <?php if($key % 3 === 0 && $key !== 0):?>
+        <div>
+            <div class="break"></div>
+        </div>
+        <?php endif;?>
         <img src="<?= str_replace(" ", "", $image['img_url']) ?>" alt="<?= $image['img_url'] ?>">
         <div>
         <?php foreach ($prices as $price): ?>
