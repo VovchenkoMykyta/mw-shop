184bef75d237e40893b98f39b17eb52c0bf280ae
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-07-21 18:39:23 +0300

    edit show products on index

--- app/views/index_index_view.php
+++ app/views/index_index_view.php
@@ -12,12 +12,18 @@ $prices = $model->getPrice();
         </div>
         <?php endif;?>
         <img src="<?= str_replace(" ", "", $image['img_url']) ?>" alt="<?= $image['img_url'] ?>">
-        <div>
-        <?php foreach ($prices as $price): ?>
-            <?php if ($image['product_id'] === $price['id']): ?>
-                <?= $price['price'] ?>
-            <?endif;?>
-        <?php endforeach; ?>
+        <div class="footer_product">
+            <form action="" method="post">
+                <input type="hidden" name="id" value="<?= $image['product_id']?>"/>
+                <input type="submit" value="Buy" id="buy_input">
+            </form>
+            <div>
+                <?php foreach ($prices as $price): ?>
+                    <?php if ($image['product_id'] === $price['id']): ?>
+                        <?= $price['price'] ?>
+                    <?endif;?>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
 <?php endforeach; ?>
